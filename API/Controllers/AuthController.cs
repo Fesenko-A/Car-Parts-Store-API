94f9b6d12fb8256dd03ff81b466490a4071e9d8d
Anton Fesenko <tohafesenkolovep@gmail.com>
2024-01-12 16:25:20 +0200

    AuthController returns ApiResponse

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -3,34 +3,43 @@ using BL;
 using BL.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace API.Controllers {
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class AuthController : ControllerBase {
         private readonly AuthBL _bl;
+        private readonly ApiResponse _response;
 
         public AuthController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager) {
             _bl = new AuthBL(userManager, roleManager);
+            _response = new ApiResponse();
         }
 
         [HttpPost]
         public async Task<ActionResult> Register(RegisterRequestDto registerRequest) {
             bool isSuccess = await _bl.Register(registerRequest);
             if (!isSuccess) {
-                return BadRequest();
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
             }
 
-            return Ok();
+            return Ok(_response);
         }
 
         [HttpPost]
         public async Task<ActionResult> Login(LoginRequestDto loginRequest) {
             LoginResponseDto? loginResponse = await _bl.Login(loginRequest);
             if (loginResponse == null) {
-                return BadRequest();
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
             }
-            return Ok(loginResponse);
+
+            _response.Result = loginResponse;
+            return Ok(_response);
         }
     }
 }
