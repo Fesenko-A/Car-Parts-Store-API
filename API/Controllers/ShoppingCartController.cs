56cecb7c3b517a1444914d9b64365ceea4560402
Anton Fesenko <tohafesenkolovep@gmail.com>
2024-01-12 16:36:34 +0200

    ShoppingCartController returns ApiResponse

--- API/Controllers/ShoppingCartController.cs
+++ API/Controllers/ShoppingCartController.cs
@@ -1,35 +1,44 @@
 ﻿using BL;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace API.Controllers {
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class ShoppingCartController : ControllerBase {
         private readonly ShoppingCartBL _bl;
+        private readonly ApiResponse _response;
+
         public ShoppingCartController() {
             _bl = new ShoppingCartBL();
+            _response = new ApiResponse();
         }
 
         [HttpGet]
-        public async Task<ActionResult> Get(string userId) {
+        public async Task<ActionResult<ApiResponse>> Get(string userId) {
             var shoppingCart = await _bl.Get(userId);
 
             if (shoppingCart == null) {
-                return BadRequest();
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
             }
 
-            return Ok(shoppingCart);
+            _response.Result = shoppingCart;
+            return Ok(_response);
         }
 
         [HttpPost]
-        public async Task<ActionResult> Upsert(string userId, int productId, int updateQuantityBy) {
+        public async Task<ActionResult<ApiResponse>> Upsert(string userId, int productId, int updateQuantityBy) {
             bool isSuccess = await _bl.Upsert(userId, productId, updateQuantityBy);
 
             if (!isSuccess) {
-                return BadRequest();
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
             }
 
-            return Ok(true);
+            return Ok(_response);
         }
     }
 }
